ba3ce01e014a8bf177490415eb6a046c2aae38d6
perebashka <kperebaieva@gmail.com>
2020-04-30 02:18:19 +0300

    added guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,15 +1,39 @@
-#!/bin/bash bash
+#!/usr/bin/env bash
+
+function num_check {
+  case $response in
+      *[!0-9]*|"") 
+            echo "\Incorrect value. Type number:"
+            read response;;
+      *) 
+            checked_num=1;;
+  esac
+}
 
 function one {
-  number=$(ls -1 | wc -l)
+  number=$(ls -aF -1 | grep -v '/' | wc -l)
   echo "How many files are in the current directory? Type number:"
   read response
+  
+  checked_num=0
+  while [[ $checked_num -eq 0 ]]
+  do
+    num_check
+  done
+  
   if [[ $response -ne $number ]] 
   then
     echo "No. Try again."
+    if [[ $number -lt $response ]] 
+    then
+      echo "Value is lower than expected."
+    else
+      echo "Value is higher than expected."
+    fi
+    
   else
     checked=1
-    echo "Yapp!"
+    echo "Congratulations! You guessed right :)"
   fi
   return
 }
